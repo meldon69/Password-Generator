24719f46b44209be7ff8b04618b451b7282747ab
meldon69 <102337958+meldon69@users.noreply.github.com>
2022-07-02 08:15:29 +1200

    Add files via upload

--- Password Generator/index.js
+++ Password Generator/index.js
@@ -10,34 +10,41 @@ function char() {
 // ==== GENERATES 4 RANDOM PASSWORDS ====
 function fourWords() {
    clear()
+
    // ==== 1st Password ====
-   let word = [char(), char(), char(), char(), char(), char(), char()]
+   let word = [char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char()]
    
-   for (i = 0; i < word.length; i++) {
+   for (i = 0; i < passwordLength(); i++) {
    let wordEl = document.querySelector("#word")
    wordEl.textContent += word[i]
    }
+   
    // ==== 2nd Password ====
-   let word1 = [char(), char(), char(), char(), char(), char(), char()]   
-   for (i = 0; i < word1.length; i++) {
+   let word1 = [char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char()]   
+
+   for (i = 0; i < passwordLength(); i++) {
    let word1El = document.querySelector("#word1")
    word1El.textContent += word1[i]
    }
+
    // ==== 3rd Password ====
-   let word2 = [char(), char(), char(), char(), char(), char(), char()]   
-   for (i = 0; i < word2.length; i++) {
+   let word2 = [char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char()]   
+
+   for (i = 0; i < passwordLength(); i++) {
    let word2El = document.querySelector("#word2")
    word2El.textContent += word2[i]
    }
+
    // ==== 4th Password ====
-   let word3 = [char(), char(), char(), char(), char(), char(), char()]   
-   for (i = 0; i < word3.length; i++) {
+   let word3 = [char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char()]
+
+   for (i = 0; i < passwordLength(); i++) {
    let word3El = document.querySelector("#word3")
    word3El.textContent += word3[i]   
    }   
 }
 
-// ==== CLEARS PREVIOUS PASSWORDS ====
+// ==== CLEARS PREVIOUS PASSWORDS + CLIPBOARD MESSAGE ====
 function clear() {
    let wordEl = document.querySelector("#word")
    wordEl.textContent = ""
@@ -47,4 +54,55 @@ function clear() {
    word2El.textContent = ""
    let word3El = document.querySelector("#word3")
    word3El.textContent = ""
+   // copied = document.querySelector("#copy")
+   // copied.textContent = ""
+}
+
+// ==== PASSWORD LENGTH ====
+function passwordLength() {
+   wordLength = document.querySelector("#pass-num").value
+   err = document.querySelector("#error")
+   if (wordLength < 4) {
+      wordLength = NaN
+     err.textContent = "Please enter a number between 4 - 20"     
+   } else if (wordLength > 20) {
+      wordLength - NaN
+      err.textContent = "Please enter a number between 4 - 20"
+   } else {
+      err.textContent = ""
+      return wordLength      
+   }   
+}
+
+// ==== COPY TO CLIPBOARD ====
+function clipboard() {   
+   copyWord = document.querySelector("#word")
+   copyWord.select()
+   copyWord.setSelectionRange(0, 99999)
+   navigator.clipboard.writeText(copyWord.value)
+   alert ("Password has been copied to clipboard")
+}
+
+function clipboard1() {   
+   copyWord = document.querySelector("#word1")
+   copyWord.select()
+   copyWord.setSelectionRange(0, 99999)
+   navigator.clipboard.writeText(copyWord.value)   
+   alert ("Password has been copied to clipboard")   
+}
+
+function clipboard2() {   
+   copyWord = document.querySelector("#word2")
+   copyWord.select()
+   copyWord.setSelectionRange(0, 99999)
+   navigator.clipboard.writeText(copyWord.value)   
+   alert ("Password has been copied to clipboard")  
+}
+
+function clipboard3() {
+   copyWord = document.querySelector("#word3")
+   copyWord.select()
+   copyWord.setSelectionRange(0, 99999)
+   navigator.clipboard.writeText(copyWord.value)   
+   alert ("Password has been copied to clipboard")
 }
