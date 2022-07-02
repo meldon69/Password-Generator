304635d191cfe37925898f1358633e77fa853e4b
meldon69 <102337958+meldon69@users.noreply.github.com>
2022-07-03 10:33:21 +1200

    Add files via upload

--- Password Generator/index.js
+++ Password Generator/index.js
@@ -1,5 +1,5 @@
 // ==== AVAILABLE CHARACTERS ====
-let characters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "&", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+const characters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "&", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 // ==== GENERATE A RANDOM CHARACTER ====
 function char() {
@@ -12,35 +12,35 @@ function fourWords() {
    clear()
 
    // ==== 1st Password ====
-   let word = [char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char()]
+   let word = ""
    
    for (i = 0; i < passwordLength(); i++) {
    let wordEl = document.querySelector("#word")
-   wordEl.textContent += word[i]
+   wordEl.textContent += word + char()
    }
    
    // ==== 2nd Password ====
-   let word1 = [char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char()]   
+   let word1 = ""   
 
    for (i = 0; i < passwordLength(); i++) {
    let word1El = document.querySelector("#word1")
-   word1El.textContent += word1[i]
+   word1El.textContent += word1 + char()
    }
 
    // ==== 3rd Password ====
-   let word2 = [char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char()]   
+   let word2 = ""   
 
    for (i = 0; i < passwordLength(); i++) {
    let word2El = document.querySelector("#word2")
-   word2El.textContent += word2[i]
+   word2El.textContent += word2 + char()
    }
 
    // ==== 4th Password ====
-   let word3 = [char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char(), char()]
+   let word3 = ""
 
    for (i = 0; i < passwordLength(); i++) {
    let word3El = document.querySelector("#word3")
-   word3El.textContent += word3[i]   
+   word3El.textContent += word3 + char()
    }   
 }
 
@@ -54,8 +54,6 @@ function clear() {
    word2El.textContent = ""
    let word3El = document.querySelector("#word3")
    word3El.textContent = ""
-   // copied = document.querySelector("#copy")
-   // copied.textContent = ""
 }
 
 // ==== PASSWORD LENGTH ====
@@ -75,34 +73,29 @@ function passwordLength() {
 }
 
 // ==== COPY TO CLIPBOARD ====
-function clipboard() {   
-   copyWord = document.querySelector("#word")
+function clip() {
    copyWord.select()
    copyWord.setSelectionRange(0, 99999)
    navigator.clipboard.writeText(copyWord.value)
    alert ("Password has been copied to clipboard")
 }
 
+function clipboard() {   
+   copyWord = document.querySelector("#word")
+   clip()
+}
+
 function clipboard1() {   
    copyWord = document.querySelector("#word1")
-   copyWord.select()
-   copyWord.setSelectionRange(0, 99999)
-   navigator.clipboard.writeText(copyWord.value)   
-   alert ("Password has been copied to clipboard")   
+   clip()  
 }
 
 function clipboard2() {   
    copyWord = document.querySelector("#word2")
-   copyWord.select()
-   copyWord.setSelectionRange(0, 99999)
-   navigator.clipboard.writeText(copyWord.value)   
-   alert ("Password has been copied to clipboard")  
+   clip() 
 }
 
 function clipboard3() {
    copyWord = document.querySelector("#word3")
-   copyWord.select()
-   copyWord.setSelectionRange(0, 99999)
-   navigator.clipboard.writeText(copyWord.value)   
-   alert ("Password has been copied to clipboard")
+   clip()
 }
